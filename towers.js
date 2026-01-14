635313060bf6443dcb1b51b5806d4026ff8ebe86
Evgeniy-makdak <makdak23@mail.ru>
2026-01-14 10:19:07 +0300

    Создал условия добавления нового города, проверку на соответствие правилу и проверку на уже названный ранее город. Убрал зависимость от регистра.

--- towers.js
+++ towers.js
@@ -4,16 +4,24 @@ const answer = document.getElementById("message");
 const resultTowers = [];
 
 input.addEventListener("blur", function newTower() {
-    if (!resultTowers) {
-        resultTowers.push(input.value)
-    } 
-    if (resultTowers.length > 0) {
-        if (resultTowers[resultTowers.length - 1].slice(-1) !== input.value[0]) {
+    const inputValue = input.value.toLowerCase().trim();
+    if (resultTowers.length === 0) {
+        resultTowers.push(inputValue)
+        alert(`Введите город на букву ${resultTowers[resultTowers.length - 1].slice(-1)}`);
+        input.value = "";
+    }
+    else {
+        console.log(resultTowers)
+        if (resultTowers[resultTowers.length - 1].slice(-1) !== inputValue[0]) {
             alert(`Первая буква вашего города не соответствует последней букве города ${resultTowers[resultTowers.length - 1]}`);
-            input.textContent = "";
+            input.value = "";
+        } else if (resultTowers.includes(inputValue)) {
+            alert(`Город ${inputValue} ухе называли!`)
+            input.value = "";
+        } else {
+            resultTowers.push(inputValue);
+            alert(`Добавлен город ${resultTowers[resultTowers.length - 1]}, введите название города на букву ${resultTowers[resultTowers.length - 1].slice(-1)}`);
+            input.value = "";
         }
-    } else {
-        resultTowers.push(input.value);
-        alert(`Добавлен город ${resultTowers[resultTowers.length - 1]}, введите название города на букву ${resultTowers[resultTowers.length - 1].slice(-1)}`);
     }
 })
